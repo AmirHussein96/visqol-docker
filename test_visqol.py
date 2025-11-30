bd724d05e95757c7c736c4c2f8a1b7ff23621832
Amir Hussein <amirnasser96@gmail.com>
2025-11-30 07:05:36 -0800

    add testing

--- test_visqol.py
+++ test_visqol.py
@@ -5,4 +5,5 @@ signal_path = "/data/test.wav"
 recons_path = "/data/test.wav"
 signal = AudioSignal(signal_path)
 recons = AudioSignal(recons_path)
-print(f"testing visqol: {metrics.quality.visqol(signal, recons, "speech")}")
+visqol_score = metrics.quality.visqol(signal, recons, 'speech')
+print(f"testing visqol: {visqol_score}")
