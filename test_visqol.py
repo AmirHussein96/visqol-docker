8e5d9c9e66ae745114646f000fc8345f1f3de54c
Amir Hussein <36240131+AmirHussein96@users.noreply.github.com>
2025-12-02 16:26:50 -0500

    Update test_visqol.py

--- test_visqol.py
+++ test_visqol.py
@@ -1,9 +1,18 @@
 from audiotools import AudioSignal
 from audiotools import metrics
+from train import losses
+
+mel_loss = losses.MelSpectrogramLoss()
+sisdr_loss = losses.SISDRLoss()
+
 
 signal_path = "/data/test.wav"
 recons_path = "/data/test.wav"
 signal = AudioSignal(signal_path)
 recons = AudioSignal(recons_path)
 visqol_score = metrics.quality.visqol(signal, recons, 'speech')
+sisdr = sisdr_loss(signal, recons)
+mel = mel_loss(signal, recons)
 print(f"testing visqol: {visqol_score}")
+print(f"testing mel: {mel}")
+print(f"testing sisdr: {sisdr}")
